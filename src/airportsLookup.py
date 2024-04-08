ad46f0434d104aebc3701cb02b72ae595b1bdade
thubular <63025159+thubular@users.noreply.github.com>
2024-04-08 10:08:40 -0700

    Update airportsLookup

--- src/airportsLookup.py
+++ src/airportsLookup.py
@@ -1,2 +1,20 @@
 # Create a lookup table (maybe a simple dict) for airports 
 # a key could be IATA code and a value could be a name of airport
+
+def airportLookup():
+    path = "../data/airports.txt"
+    mydict = {}
+
+    with open(path, encoding="utf8") as filedata:
+        for line in filedata.readlines():
+            line = line.split(',')
+            key = line[1].strip('"')
+            value = line[4].strip('"')
+            if key in mydict:
+                mydict[key].append(value)
+            else:
+                mydict[key] = [value]
+    return mydict
+
+
+
