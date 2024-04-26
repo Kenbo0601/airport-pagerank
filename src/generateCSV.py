6d690a6a8a7af601eb6b6b4b58decb21e47226fa
Kenbo0601 <eug.ken0601@gmail.com>
2024-04-26 01:33:55 -0700

    small change

--- src/generateCSV.py
+++ src/generateCSV.py
@@ -6,6 +6,8 @@ input_file_path = 'source-dest.txt'
 # Path to the output CSV file
 output_file_path = 'source-dest.csv'
 
+col_names = ["source", "target"]
+
 # Read the text file and split the lines into a list, skipping empty lines
 with open(input_file_path, 'r') as file:
     lines = [line.strip() for line in file if line.strip()]
@@ -13,6 +15,7 @@ with open(input_file_path, 'r') as file:
 # Write the list to a CSV file
 with open(output_file_path, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(col_names)
     for line in lines:
         # Split each line by white space (spaces or tabs) and write it to the CSV file
         csvwriter.writerow(line.split())
